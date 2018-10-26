e883dcba6849b3c60ac7023cde8675a665d375bb
Ben Patterson <bmizepatterson@gmail.com>
2018-10-26 15:25:44 -0400

    Add sidebar and topbar components

---
+++ resources/views/component/sidebar.blade.php
@@ -0,0 +1,16 @@
+<nav class="col-md-3 d-none d-md-block bg-light sidebar">
+    <div class="sidebar-sticky">
+        <img src="#" width="150" height="217" class="mx-auto d-block">
+        <p class="text-center">Journal 1</p>
+
+        <div>
+            <h6 class="mx-3 mt-5">In this journal:</h6>
+            <div class="list-group list-group-flush border-right border-bottom">
+                <a class="list-group-item list-group-item-action" href="#"><i class="fas fa-user user-pic"></i>Bobbert Bob</a>
+                <a class="list-group-item list-group-item-action active" href="#"><i class="fas fa-user user-pic"></i>Bobby Bob</a>
+                <a class="list-group-item list-group-item-action" href="#"><i class="fas fa-user user-pic"></i>Bonnie Bobbington</a>
+                <a class="list-group-item list-group-item-action" href="#"><i class="fas fa-user user-pic"></i>Boris Bobford</a>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/journal/read.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.page')
+
+@section('title', 'Read')
+
+@section('page-content')
+<div class="sticky-top pt-5 bg-white border-bottom">
+    <div class="float-right">
+        <a class="text-muted" href="#"><i class="fas fa-edit ml-3"></i></a>
+    </div>
+    <h1 class="entry-title">{{ $journal['title'] }}</h1>
+
+</div>
+<div class="row no-gutters">
+    <div class="col-lg-8">
+        <div class="entry-body">{!! $journal['body'] !!}</div>
+    </div>
+
+    <div class="d-none d-lg-block col-lg-4">
+        (Comment area)
+    </div>
+</div>
+@endsection
