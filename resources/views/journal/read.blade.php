af5edb2dad9a498fa4e2da0c14b55d46a47540b5
Ben Patterson <bmizepatterson@gmail.com>
2018-10-26 16:05:16 -0400

    Add entry navigation to read layout

--- resources/views/journal/read.blade.php
+++ resources/views/journal/read.blade.php
@@ -4,15 +4,27 @@
 
 @section('journal-content')
 <div class="sticky-top pt-5 bg-white border-bottom">
-    <div class="float-right">
-        <a class="text-muted" href="#"><i class="fas fa-edit ml-3"></i></a>
+
+    <nav class="nav justify-content-between">
+        <a class="nav-item nav-link" href="#"><i class="fas fa-backward mr-2"></i>Previous Entry</a>
+        <a class="nav-item nav-link" href="#">Table of Contents</a>
+        <a class="nav-item nav-link" href="#">Next Entry<i class="fas fa-forward ml-2"></i></a>
+    </nav>
+
+    <div class="float-right m-2">
+        <a class="text-muted" href="#"><i class="fas fa-edit"></i></a>
     </div>
-    <h1 class="entry-title">{{ $journal['title'] }}</h1>
+
+    <div class="m-2">
+        <h1 class="entry-title mb-0">{{ $journal['title'] }}</h1>
+        <small class="entry-meta text-muted">Posted on Friday, October 26 at 3:59 PM by <a href="#">Bobby Bob</a></small>
+    </div>
 
 </div>
+
 <div class="row no-gutters">
     <div class="col-lg-8">
-        <div class="entry-body">{!! $journal['body'] !!}</div>
+        <div class="entry-body pl-2">{!! $journal['body'] !!}</div>
     </div>
 
     <div class="d-none d-lg-block col-lg-4">
