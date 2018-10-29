7563903f66c060a7a36382817ff01bd6d40b20f2
Ben Patterson <bmizepatterson@gmail.com>
2018-10-29 11:47:26 -0400

    Add nav links to journal.index view

--- resources/views/journal/index.blade.php
+++ resources/views/journal/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('page-content')
 <main class="container">
-    <h1 class="mb-4">My Journals</h1>
+    <h1 class="my-5">My Journals</h1>
     <div class="row">
     @foreach ($journals as $journal)
         <div class="col-md">
@@ -15,12 +15,17 @@
                             <a href="#"></a>
                         </div>
                         <div class="col-md">
-                            <h2 class="card-title"><a href="#">{{ $journal['title'] }}</a></h2>
+                            <h2 class="card-title"><a href="/journal/read">{{ $journal['title'] }}</a></h2>
+                            <nav class="nav flex-column">
+                                <a class="nav-link py-1" href="/journal/write"><i class="fas fa-fw fa-plus mr-2"></i>New post</a>
+                                <a class="nav-link py-1" href="/journal/read"><i class="fas fa-fw fa-book-reader mr-2"></i>Read latest</a>
+                                <a class="nav-link py-1" href="/journal/contents"><i class="fab fa-fw fa-readme mr-2"></i>All entries</a>
+                            </nav>
                         </div>
                     </div>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><h5 class="m-0">Participants</h5></li>
+                    <li class="list-group-item"><h5 class="m-0">People in this journal:</h5></li>
                     @foreach ($journal['participants'] as $participant)
                         <li class="list-group-item"><a href="#"><i class="fas fa-user user-pic"></i>{{ $participant }}</a></li>
                     @endforeach
