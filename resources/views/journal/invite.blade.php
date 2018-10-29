35764833e3c3c5422219c806f0d48607d49e2803
Ben Patterson <bmizepatterson@gmail.com>
2018-10-29 15:21:25 -0400

    Add 'invite' button to journal.invite view

--- resources/views/journal/invite.blade.php
+++ resources/views/journal/invite.blade.php
@@ -5,12 +5,15 @@
 @section('page-content')
 <main class="container p-md-5">
     <h1>Invite a friend to join {{ $journal['title'] }}</h1>
+    <p class="font-italic">{{ $journal['description'] }}</p>
     <form method="post" action="">
         <div class="form-group my-5">
             <label for="email">Email address:</label>
             <input type="email" class="form-control" id="email" name="title">
         </div>
-
+        <div class="form-group my-5">
+            <button type="submit" class="btn btn-block btn-primary">Invite</button>
+        </div>
     </form>
 </main>
 @endsection
