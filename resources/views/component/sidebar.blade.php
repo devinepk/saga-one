2dc3ee33e9b3ea3e7a463cd5381471ded5fcede0
Ben Patterson <bmizepatterson@gmail.com>
2018-10-29 11:47:15 -0400

    Add dummy link to sidebar

--- resources/views/component/sidebar.blade.php
+++ resources/views/component/sidebar.blade.php
@@ -1,7 +1,7 @@
 <nav class="col-md-3 d-none d-md-block bg-light sidebar">
     <div class="sidebar-sticky">
         <img src="/img/cover1.jpg" width="150" height="217" class="mx-auto d-block">
-        <h5 class="text-center mt-1">Journal 1</h5>
+        <h5 class="text-center mt-1"><a href="/journal/contents">Journal 1</a></h5>
 
         <div>
             <h6 class="mx-3 mt-5">In this journal:</h6>
