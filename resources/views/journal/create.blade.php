79b31cf215b6ef2eb268bba2f37c12a9be5888f4
Ben Patterson <bmizepatterson@gmail.com>
2018-10-29 11:06:57 -0400

    Adjust spacing in journal.create

--- resources/views/journal/create.blade.php
+++ resources/views/journal/create.blade.php
@@ -3,36 +3,33 @@
 @section('page-title', 'Create a new journal')
 
 @section('page-content')
-<main class="container">
-    <h1 class="mb-4">Create a new journal</h1>
-    <form method="post" action="" class="m-5">
-        <div class="form-group">
-            <label for="title">Journal title:</label>
+<main class="container p-md-5">
+    <h1>Create a new journal</h1>
+    <form method="post" action="">
+        <div class="form-group my-5">
+            <label for="title">Give this journal a title:</label>
             <input class="form-control" id="title" name="title">
         </div>
-        <div class="form-group">
+        <div class="form-group my-5">
             <div class="custom-file">
+                <label class="custom-file-label" for="cover_img">Upload a cover image for this journal</label>
                 <input type="file" class="custom-file-input" id="cover_img" name="cover_img">
-                <label class="custom-file-label" for="cover_img">Upload a journal cover</label>
             </div>
         </div>
-        <div class="form-group">
-            <label for="participants">Select participants:</label>
-            <select id="participants" name="participants" class="form-control" size="10" multiple>
-                @foreach ($friends as $friend)
-                    <option selected>{{ $friend }}</option>
-                @endforeach
-            </select>
+        <div class="form-group my-5">
+            <label for="participants">Add some friends to this journal:</label>
+            <input type="text" id="participants" class="form-control" placeholder="Search friends">
         </div>
-        <div class="form-group">
+        <div class="form-group my-5">
             <label>Set rotation order:</label>
             <ul class="list-group">
+                <li class="list-group-item list-group-item-action active">Bobby Bob (You)</li>
                 @foreach ($friends as $friend)
-                    <li class="list-group-item">{{ $friend }}</li>
+                    <li class="list-group-item list-group-item-action">{{ $friend }}</li>
                 @endforeach
             </ul>
         </div>
-        <div class="form-group">
+        <div class="form-group my-5">
             <button type="submit" class="btn btn-block btn-primary">Create</button>
         </div>
     </form>
