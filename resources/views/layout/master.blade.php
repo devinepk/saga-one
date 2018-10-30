e6ca0efcb4dff648f3e35fd28390a32ba30e2e4e
Ben Patterson <bmizepatterson@gmail.com>
2018-10-30 10:36:03 -0400

    Correct link to JS on master layout

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -12,7 +12,7 @@
 <body class="@yield('body_classes')">
     @yield('body')
 
-    <script src="js/app.js"></script>
+    <script src="/js/app.js"></script>
     @yield('bottom_of_body')
 </body>
 </html>
