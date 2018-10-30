ef624585bfad493485296b50dd73297b3da95505
Ben Patterson <bmizepatterson@gmail.com>
2018-10-30 10:28:45 -0400

    Style user menu dropdown

--- resources/views/component/topbar.blade.php
+++ resources/views/component/topbar.blade.php
@@ -1,14 +1,14 @@
 <nav class="navbar navbar-dark fixed-top bg-dark p-0 shadow">
-    <a class="brand navbar-brand col-sm-3 col-md-2 mr-0" href="/"><span class="saga">Saga</span>one</a>
-    <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#"><i class="fas fa-user mr-2"></i>Bobby Bob</a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-        </li>
-    </ul>
+    <a class="brand navbar-brand mx-3" href="/"><span class="saga">Saga</span>one</a>
+
+    <div class="dropdown">
+        <a class="nav-link text-light dropdown-toggle" id="userMenuLabel" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#"><i class="fas fa-user mr-2"></i>Bobby Bob</a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userMenuLabel">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </div>
 </nav>
