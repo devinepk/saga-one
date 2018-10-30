402cc4ca15e3db9a0d8ba45209945ebe17a596aa
Ben Patterson <bmizepatterson@gmail.com>
2018-10-30 15:18:29 -0400

    Add a countdown and notification to journal.index

--- resources/views/journal/index.blade.php
+++ resources/views/journal/index.blade.php
@@ -4,11 +4,22 @@
 
 @section('journal-content')
 <div class="container p-md-5">
+
+    <div class="float-right m-2 text-right">
+        <button type="button" class="btn btn-info">Deliver this journal to Bobbert<i class="fas fa-arrow-alt-circle-right ml-2"></i></button>
+    </div>
+
     <h1>{{ $journal['title'] }}</h1>
     <p class="font-italic">{{ $journal['description'] }}</p>
-    @if ($journal['participantCount'])
-        <p>Shared with {{ $journal['participantCount'] }} friends.</p>
-    @endif
+
+    <div class="alert alert-warning alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <strong>Time is almost out!</strong> Your time with this journal will end in 23 hours.
+    </div>
+
+    <table class="mb-3"><tr><th>Countdown:</th><td>23 hours, 23 minutes</td></tr></table>
 
     <h2>Latest entries</h2>
     @foreach ($entries as $entry)
