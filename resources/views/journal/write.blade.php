5ca13edc1f6229a11c12772c47774b844dcae7bf
Ben Patterson <bmizepatterson@gmail.com>
2018-10-31 10:04:18 -0400

    Use entry-header component in journal.write

--- resources/views/journal/write.blade.php
+++ resources/views/journal/write.blade.php
@@ -7,12 +7,9 @@
 @section('page-title', 'Edit Entry')
 
 @section('journal-content')
-<div class="sticky-top pt-5 bg-white border-bottom">
-    <div class="ml-2 mr-2">
-        <h1 class="entry-title">{{ $entry['title'] }}</h1>
-        {{-- <small class="entry-meta text-muted">Posted on Friday, October 26 at 3:59 PM by <a href="#">Bobby Bob</a></small> --}}
-    </div>
-</div>
+<entry-header>
+    {{ $entry['title'] }}
+</entry-header>
 
 <div id="edit-content" class="">
     <div id="editor" class="p-2">
