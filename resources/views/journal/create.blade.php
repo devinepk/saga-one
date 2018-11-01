9fd0f46c61de5c4f6b38c9de57f87b6c10a15850
Ben Patterson <bmizepatterson@gmail.com>
2018-11-01 16:59:31 -0400

    Update journal.create form

--- resources/views/journal/create.blade.php
+++ resources/views/journal/create.blade.php
@@ -4,42 +4,33 @@
 
 @section('page-content')
 <main class="container p-md-5">
-    <h1>Create a new journal</h1>
-    <form method="post" action="/journal/">
-        @csrf
-        <input type="hidden" name="action" value="store">
-        <div class="form-group my-5">
-            <label for="title">Give this journal a title:</label>
-            <input class="form-control" id="title" name="title">
-        </div>
-        <div class="form-group my-5">
-            <label for="description">Give this journal a short description:</label>
-            <input type="text" class="form-control" id="description" name="description">
-        </div>
-        <div class="form-group my-5">
-            <div class="custom-file">
-                <label class="custom-file-label" for="cover_img">Upload a cover image for this journal</label>
-                <input type="file" class="custom-file-input" id="cover_img" name="cover_img">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><h1 class="mb-0">Create a new journal</h1></div>
+                <div class="card-body">
+                    <form method="post" action="{{ route('journal.store') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="title">Give this journal a title:</label>
+                            <input class="form-control" id="title" name="title">
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Give this journal a short description:</label>
+                            <input type="text" class="form-control" id="description" name="description">
+                        </div>
+                        <div class="row pt-3">
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-block btn-link" formaction="{{ route('journal.index') }}">Cancel</a>
+                            </div>
+                            <div class="col">
+                                <button type="submit" class="btn btn-block btn-primary">Create</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-        {{--
-        <div class="form-group my-5">
-            <label for="participants">Add some friends to this journal:</label>
-            <input type="text" id="participants" class="form-control" placeholder="Search friends">
-        </div>
-        <div class="form-group my-5">
-            <label>Set rotation order:</label>
-            <ul class="list-group">
-                <li class="list-group-item list-group-item-action active">Bobby Bob (You)</li>
-                @foreach ($friends as $friend)
-                    <li class="list-group-item list-group-item-action">{{ $friend }}</li>
-                @endforeach
-            </ul>
-        </div>
-        --}}
-        <div class="form-group my-5">
-            <button type="submit" class="btn btn-block btn-primary">Create</button>
-        </div>
-    </form>
+    </div>
 </main>
 @endsection
