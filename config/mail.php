5ae812449a4685f05304c447a7deaf7dc6260d7d
Ben Patterson <bmizepatterson@gmail.com>
2018-11-12 20:41:54 -0500

    Set default "from" address

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => 'no-reply@saga-one.com',
+        'name' => 'SagaOne',
     ],
 
     /*
