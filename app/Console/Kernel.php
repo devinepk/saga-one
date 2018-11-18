54a1eb21ae16fc6337a7afc2adc3912094bc77bf
Ben Patterson <bmizepatterson@gmail.com>
2018-11-18 15:55:37 -0500

    Rotate journals every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\RegisteredUsers'
+        'App\Console\Commands\RegisteredUsers',
+        'App\Console\Commands\RotateJournals'
     ];
 
     /**
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('registered:users')->everyMinute();
+        $schedule->command('registered:users')->daily()->environments(['staging', 'production']);
         $schedule->command('journals:rotate')->everyMinute();
     }
 
