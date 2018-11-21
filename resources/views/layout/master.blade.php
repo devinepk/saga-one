91656737d52d7ffc10816f6c80f58d846fcfa3f0
Ben Patterson <bmizepatterson@gmail.com>
2018-11-20 19:12:26 -0500

    Remove unneeded dns-prefetch header from master layout

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="dns-prefetch" href="https://fonts.googleapis.com/">
     <link rel="dns-prefetch" href="https://fonts.gstatic.com/">
     @yield('additional_link_tags')
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
