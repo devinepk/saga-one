6f73ab73be7405c3626136916b9fc53c4bd1589f
Ben Patterson <bmizepatterson@gmail.com>
2018-11-22 16:24:13 -0500

    Don't enforce https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,9 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') == 'production') {
-            URL::forceScheme('https');
-        }
+        // if (env('APP_ENV') == 'production') {
+        //     URL::forceScheme('https');
+        // }
 
         VerifyEmail::toMailusing(function ($notifiable) {
             $verificationUrl = URL::temporarySignedRoute(
