f76d0876325b9388f66487d65a93e43f58210fb4
Ben Patterson <bmizepatterson@gmail.com>
2018-11-25 15:32:35 -0500

    Fix card-footer in journal.create

--- resources/views/journal/create.blade.php
+++ resources/views/journal/create.blade.php
@@ -8,7 +8,7 @@
         <div class="card">
             <div class="card-header"><h1 class="mb-0">Create a new journal</h1></div>
             <div class="card-body">
-                <form method="post" action="{{ route('journal.store') }}">
+                <form method="post" action="{{ route('journal.store') }}" id="create-form">
                     @csrf
                     <div class="form-group">
                         <label for="title">Give this journal a title:</label>
@@ -23,15 +23,17 @@
                         <label for="description">Give this journal a short description:</label>
                         <input type="text" class="form-control" id="description" name="description">
                     </div>
-                    <div class="row pt-3">
-                        <div class="col-4">
-                            <a class="btn btn-block" href="{{ route('journal.index') }}">Cancel</a>
-                        </div>
-                        <div class="col">
-                            <button type="submit" class="btn btn-block btn-primary">Create</button>
-                        </div>
-                    </div>
                 </form>
+            </div>
+            <div class="card-footer p-0">
+                <div class="row no-gutters">
+                    <div class="col-4">
+                        <a class="btn btn-block" href="{{ route('journal.index') }}">Cancel</a>
+                    </div>
+                    <div class="col">
+                        <button type="submit" form="create-form" class="btn btn-block btn-primary">Create</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
