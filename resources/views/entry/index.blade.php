c08dbdfbefa04c1a1f39264d51cec1c69e5aa514
Ben Patterson <bmizepatterson@gmail.com>
2018-11-25 16:22:36 -0500

    Fix typo in journal cover img link

--- resources/views/entry/index.blade.php
+++ resources/views/entry/index.blade.php
@@ -29,7 +29,7 @@
         auth-user-json="{{ Auth::user() }}"
         read-url="{{ Auth::user()->can('view', $journal) ? route('journal.contents', $journal) : '' }}"
         write-url="{{ Auth::user()->can('addEntry', $journal) ? route('journal.show', $journal) : '' }}"
-        image-url="{{ Storage::url('img/cover1.png') }}"
+        image-url="{{ Storage::url('img/cover1.jpg') }}"
         settings-url="{{ Auth::user()->can('viewSettings', $journal) ? route('journal.settings', $journal) : '' }}"
         queue-json="{{ $journal->queue }}"
         journal-json="{{ $journal }}"
