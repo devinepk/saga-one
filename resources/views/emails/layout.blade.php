ddf153c869bb9d8eda543d1623b6eaeb75ff1046
Ben Patterson <bmizepatterson@gmail.com>
2018-11-27 13:10:58 -0500

    Put emails.layout styles inside the head tag

--- resources/views/emails/layout.blade.php
+++ resources/views/emails/layout.blade.php
@@ -3,8 +3,6 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-</head>
-<body>
     <style>
         @media only screen and (max-width: 600px) {
             .inner-body {
@@ -309,6 +307,8 @@
         }
 
     </style>
+</head>
+<body>
 
     <table class="wrapper" width="100%" cellpadding="0" cellspacing="0">
         <tr>
