3d9b60cb531e01b9cf80add5b1e8940e8ffba3b5
Ben Patterson <bmizepatterson@gmail.com>
2018-11-29 15:57:10 -0500

    Only forceRootURL in worker environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Force the URL generator to use the url specified in the env settings
-        URL::forceRootUrl(config('app.url'));
+        if (App::environment('worker')) {
+            // Force the URL generator to use the url specified in the env settings.
+            // Otherwise the worker uses host 'localhost' for some reason.
+            URL::forceRootUrl(config('app.url'));
+        }
 
         VerifyEmail::toMailusing(function ($notifiable) {
             $verificationUrl = URL::temporarySignedRoute(
