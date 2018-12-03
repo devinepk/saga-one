6222d7d32001aecc56bd1a4c12b063b61dbb8b4f
Ben Patterson <bmizepatterson@gmail.com>
2018-12-03 18:52:06 -0500

    Fix style bugs in entry.create and entry.show

--- resources/views/entry/create.blade.php
+++ resources/views/entry/create.blade.php
@@ -8,18 +8,24 @@
 @endsection
 
 @section('journal-content')
-<entry-header author-json="{{ Auth::user() }}">
-    <input v-focus type="text" form="entry-save-form" id="title" name="title" class="border-0 w-100 {{ $errors->has('title') ? ' is-invalid' : '' }}" style="outline:none;" value="{{ old('title') }}" placeholder="Title" autofocus>
-    @if ($errors->has('title'))
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $errors->first('title') }}</strong>
-        </span>
-    @endif
-</entry-header>
+<div class="entry-container d-flex h-100">
+    <entry-header>
+        <input v-focus type="text" form="entry-save-form" id="title" name="title" class="border-0 w-100 {{ $errors->has('title') ? ' is-invalid' : '' }}" style="outline:none;" value="{{ old('title') }}" placeholder="Title" autofocus>
+        @if ($errors->has('title'))
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('title') }}</strong>
+            </span>
+        @endif
+    </entry-header>
 
-<quill-editor></quill-editor>
+    <quill-editor></quill-editor>
 
-<entry-save-form form-id="entry-save-form" action-url="{{ route('entry.store') }}" :journal-id="{{ $journal->id }}">
-    <template slot="csrf">@csrf</template>
-</entry-save-form>
+    <entry-save-form
+        form-id="entry-save-form"
+        action-url="{{ route('entry.store') }}"
+        :journal-id="{{ $journal->id }}"
+    >
+        <template slot="csrf">@csrf</template>
+    </entry-save-form>
+</div>
 @endsection
