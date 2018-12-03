a4544d9d5e6fae6f1300bf588777aef0b12b88fa
Ben Patterson <bmizepatterson@gmail.com>
2018-12-03 14:11:33 -0500

    Conditionally show the flashMessage container div

--- resources/views/component/flashMessage.blade.php
+++ resources/views/component/flashMessage.blade.php
@@ -1,25 +1,34 @@
-<div class="container">
-    @if ($errors->any())
-        <alert level="danger">Please fix the errors below.</alert>
-    @endif
+@if ($errors->any()
+     || session('status')
+     || session('warning')
+     || session('verified')
+     || session('resent')
+     || session('invite_resent'))
 
-    @if (session('status'))
-        <alert level="primary">{!! session('status') !!}</alert>
-    @endif
+    <div class="container">
+        @if ($errors->any())
+            <alert level="danger">Please fix the errors below.</alert>
+        @endif
 
-    @if (session('warning'))
-        <alert level="warning">{!! session('warning') !!}</alert>
-    @endif
+        @if (session('status'))
+            <alert level="primary">{!! session('status') !!}</alert>
+        @endif
 
-    @if (session('verified'))
-        <alert level="primary">Thank you for verifying your email address!</alert>
-    @endif
+        @if (session('warning'))
+            <alert level="warning">{!! session('warning') !!}</alert>
+        @endif
 
-    @if (session('resent'))
-        <alert level="primary">{{ __('A fresh verification link has been sent to your email address.') }}</alert>
-    @endif
+        @if (session('verified'))
+            <alert level="primary">Thank you for verifying your email address!</alert>
+        @endif
 
-    @if (session('invite_resent'))
-        <alert level="primary">{!! session('invite_resent') !!}</alert>
-    @endif
-</div>
+        @if (session('resent'))
+            <alert level="primary">{{ __('A fresh verification link has been sent to your email address.') }}</alert>
+        @endif
+
+        @if (session('invite_resent'))
+            <alert level="primary">{!! session('invite_resent') !!}</alert>
+        @endif
+    </div>
+
+@endif
